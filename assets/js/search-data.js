20afbe7602385f8b126de692469b525dbe44899f
alvin-pc-chen <104297650+alvin-pc-chen@users.noreply.github.com>
2025-08-07 20:01:11 +0000

    Deploying to gh-pages from @ alvin-pc-chen/alvin-pc-chen.github.io@e63f660e00ed4b082390834093040361876bde9b 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,130 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "I update my projects once in a while.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "For detailed descriptions please view projects!",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "post-scanning-machine-brains",
+        
+          title: "Scanning Machine Brains",
+        
+        description: "How supervised probing can help us isolate linguistic understanding in LLMs.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/probing/";
+          
+        },
+      },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather/";
+            },},{id: "news-the-umr-project-released-the-2-0-dataset-and-the-english-corpus-now-has-over-30k-sentences",
+          title: 'The UMR Project released the 2.0 dataset and the English corpus now has...',
+          description: "",
+          section: "News",},{id: "projects-numpy-cnn",
+          title: 'numpy cnn',
+          description: "A Convolutional Neural Network implemented in NumPy with modular layers.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/cnn/";
+            },},{id: "projects-irony-detector",
+          title: 'irony detector',
+          description: "A BiLSTM that detects irony in tweets based off SemEval 2018 Task 3.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/irony/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%61%6C%76%69%6E.%63%68%65%6E@%63%6F%6C%6F%72%61%64%6F.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/alvin-pc-chen", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/alvin-pc-chen", "_blank");
+        },
+      },{
+        id: 'social-medium',
+        title: 'Medium',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://medium.com/@alch6097", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
